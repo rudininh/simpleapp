dfff19335813d103d66cf5f6b877be6996b9745c
rudininh <rudini.norhabibi@gmail.com>
2023-12-07 09:25:59 +0700

    belajar

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,13 +5,15 @@ const TodoList = ({ isRefresh, setRefresh }) => {
     const [todos, setTodos] = useState([]);
 
     useEffect(() => {
-        if (isRefresh) {
+        if (setRefresh) {
             fetch("http://localhost:8000/todos")
                 .then((res) => {
                     return res.json();
                 }).then((data) => {
+                    setRefresh(false);
                     setTodos(data);
                 }).catch((err) => {
+                    setRefresh(false);
                     if (err.name === "AbortError") {
                         console.log("fetch aborted");
                     }
@@ -22,7 +24,7 @@ const TodoList = ({ isRefresh, setRefresh }) => {
     return (
         <ul id="todo-list">
             {todos.map((todo) => (
-                <TodoItem todo={todo} key={todo.id} />
+                <TodoItem todo={todo} key={todo.id} setRefresh={setRefresh}/>
             ))}
         </ul>
     );
